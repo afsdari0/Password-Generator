283d62754ecf93a8acf53bd0e25811cf82c204e6
Dario Ramos <dariorbneto2@hotmail.com>
2022-09-08 18:42:26 -0300

    Html and Css  concluded

---
+++ script.js
@@ -0,0 +1,12 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#value");
+let password = document=querySelector("#password");
+
+let containerPassword = document.querySelector("container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+let newPassword = "";
+
+sizePassword.innerHTML = sliderElement.value;
